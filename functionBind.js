8e467c4e933cdfc71c588e94f932d6c2dc98cac8
Siwoo <postsw7@gmail.com>
2017-04-18 17:40:11 +0900

    first commit

---
+++ functionBind.js
@@ -0,0 +1,71 @@
+/*
+ * function bind():
+ *
+ * example 1:
+ *
+ * var alice = {
+ *   name: 'alice',
+ *   shout: function(){
+ *     alert(this.name);
+ *   }
+ * }
+ * var boundShout = bind(alice.shout, alice);
+ * boundShout(); // alerts 'alice'
+ * boundShout = bind(alice.shout, {name: 'bob'});
+ * boundShout(); // alerts 'bob'
+ *
+ * example 2:
+ *
+ * var func = function(a, b){ return a + b };
+ * var boundFunc = bind(func, null, 'foo');
+ * var result = boundFunc('bar');
+ * result === 'foobar'; // true
+ *
+*/
+
+var bind = function(func, that) {
+  var args = Array.prototype.slice.call(arguments, 2);
+  return function(){
+    var arr = Array.prototype.slice.call(arguments);
+    args = args.concat(arr);
+    return func.apply(that, args);
+  }
+};
+
+/*
+ * Function.prototype.bind:
+ *
+ * example 1:
+ *
+ * var alice = {
+ *   name: 'alice',
+ *   shout: function(){
+ *     alert(this.name);
+ *   }
+ * }
+ * var boundShout = alice.shout.bind(alice);
+ * boundShout(); // alerts 'alice'
+ * boundShout = alice.shout.bind({name: 'bob'});
+ * boundShout(); // alerts 'bob'
+ *
+ * example 2:
+ *
+ * var func = function(a, b){ return a + b };
+ * var boundFunc = func.bind(null, 'foo');
+ * var result = boundFunc('bar');
+ * result === 'foobar'; // true
+ *
+*/
+
+Function.prototype.bind = function(that
+) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  var save = this;
+  return function(){
+    var arr = Array.prototype.slice.call(arguments);
+    var allArgs = args.concat(arr);
+    return save.apply(that, allArgs);
+  }
+};
+
+module.exports = bind;
---
+++ spec/functionBindSpec.js
@@ -0,0 +1,50 @@
+var expect = require('chai').expect;
+var bind = require('../functionBind.js');
+
+describe('functionBind', function() {
+
+  it('should have true for result', function() {
+
+    var alice = {
+      name: 'alice',
+      shout: function(){
+        return this.name;
+      }
+    }
+    var boundShout = bind(alice.shout, alice);
+    boundShout() === 'alice'; // alerts 'alice'
+    var boundTemp = bind(alice.shout, {name: 'bob'});
+    boundTemp() === 'bob'; // alerts 'bob'
+
+    var func = function(a, b){ return a + b };
+    var boundFunc = bind(func, null, 'foo');
+    var result = boundFunc('bar');
+    ; // true
+
+    expect(boundShout() === 'alice').to.equal(true);
+    expect(boundTemp()).to.equal('bob');
+    expect(result === 'foobar').to.equal(true);
+  });
+
+  it('should have true for result', function() {
+
+    var alice = {
+      name: 'alice',
+      shout: function(){
+        return this.name;
+      }
+    }
+    var boundShout = alice.shout.bind(alice);
+    var res = boundShout() === 'alice'; // alerts 'alice'
+    boundTemp = alice.shout.bind({name: 'bob'});
+    boundTemp(); // alerts 'bob'
+
+    var func = function(a, b){ return a + b };
+    var boundFunc = func.bind(null, 'foo');
+    var result = boundFunc('bar');
+
+    expect(res).to.equal(true);
+    expect(boundTemp()).to.equal('bob');
+    expect(result === 'foobar').to.equal(true);
+  });
+});
