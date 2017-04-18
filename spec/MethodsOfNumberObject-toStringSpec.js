305e27d06de3b3884ac3e99950d6de76621b030b
Siwoo <postsw7@gmail.com>
2017-04-19 00:22:56 +0900

    trainingJS #13~14

---
+++ spec/MethodsOfNumberObject-toStringSpec.js
@@ -0,0 +1,23 @@
+var expect = require('chai').expect;
+var colorOf = require('../MethodsOfNumberObject-toString.js');
+
+describe('MethodsOfNumberObject-toString', function() {
+
+  it('should have correct hexString values', function() {
+
+    expect(colorOf(161, 218, 70)).to.equal("#a1da46");
+    expect(colorOf(71, 37, 225)).to.equal("#4725e1");
+    expect(colorOf(138, 127, 94)).to.equal("#8a7f5e");
+    expect(colorOf(71, 18, 119)).to.equal("#471277");
+    expect(colorOf(127, 255, 223)).to.equal("#7fffdf");
+  });
+
+  it('should have false for result', function() {
+
+    expect(colorOf(1,12,139) === "#123455").to.equal(false);
+    expect(colorOf(25,129,8) === "#123455").to.equal(false);
+    expect(colorOf(102,220,221) === "#123455").to.equal(false);
+    expect(colorOf(255,17,4) === "#123455").to.equal(false);
+    expect(colorOf(127,128,129) === "#123455").to.equal(false);
+  });
+});
---
+++ spec/NumberOfObjectAndItsPropertiesSpec.js
@@ -0,0 +1,15 @@
+var assert = require('chai').assert;
+var whatNumberIsIt = require('../NumberOfObjectAndItsProperties.js');
+
+describe('whatNumberIsIt', function () {
+  it('should have correct answer', function () {
+    assert.equal(whatNumberIsIt(1/0),"Input number is Number.POSITIVE_INFINITY");
+    assert.equal(whatNumberIsIt(100),"Input number is 100");
+    assert.equal(whatNumberIsIt(1.7976931348623157e+308),"Input number is Number.MAX_VALUE");
+    assert.equal(whatNumberIsIt(5e-324),"Input number is Number.MIN_VALUE");
+    assert.equal(whatNumberIsIt(-Number.MAX_VALUE*2),"Input number is Number.NEGATIVE_INFINITY");
+    assert.equal(whatNumberIsIt(NaN),"Input number is Number.NaN");
+    assert.equal(whatNumberIsIt(Infinity+1),"Input number is Number.POSITIVE_INFINITY");
+  });
+
+})
